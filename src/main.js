65578db4ba990bba2ce220a730304cc4421b2950
Mateo Aguilar <maaguilarca@unal.edu.co>
2020-10-04 18:02:41 -0500

    cambios a login para permitir acceso al backend, pero fallido

--- src/main.js
+++ src/main.js
@@ -5,8 +5,16 @@ import router from '@/router';
 import store from '@/store';
 import * as VeeValidate from 'vee-validate';
 import 'es6-promise/auto'
-
+import GAuth from 'vue-google-oauth2'
+import { ValidationProvider } from 'vee-validate';
+const gauthOption = {
+    clientId: '699443937997-uq28jn89e4d89aqpob3cc79ab4o2aa6i.apps.googleusercontent.com',
+    scope: 'profile email',
+    prompt: 'select_account'
+}
+Vue.use(GAuth, gauthOption)
 Vue.config.productionTip = false
+Vue.component('ValidationProvider', ValidationProvider);
 Vue.use(VeeValidate)
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
+  <div class="align-self-center">
     <b-container>
       <b-row class ="justify-content-md-center "><!-- para centrar en la pantalla-->
-        <b-col col md="8" >
+        <b-col col md="6" >
+          <h1>Fertig</h1>
           <b-card
               header="Login"
               border-variant="primary"
@@ -11,25 +12,37 @@
           >
             <b-card-text >
               <!-- no pasa nada hasta que se hace el 'registrarse'-->
-              <b-form @submit="onSubmit">
+              <b-form @submit.prevent="onSubmit">
                 <b-form-group
-                    label="Correo">
+                    label="username">
                   <!-- required para que no pase nada si no está lleno el campo-->
-                  <b-form-input v-model="Correo" required></b-form-input>
+                  <ValidationProvider name="username" rules="required">
+                    <b-form-input v-model="username" required  placeholder="username"></b-form-input>
+                  </ValidationProvider>
                 </b-form-group>
 
                 <b-form-group
                     label="Contraseña">
-                  <b-form-input v-model="Password"  required type="password"></b-form-input>
+                  <ValidationProvider name="password" rules="required">
+                    <b-form-input v-model="password"  required type="password" placeholder="*******"></b-form-input>
+                  </ValidationProvider>
+
                 </b-form-group>
                 <b-form-group>
                   <!--se desabilita el boton si los campos no estan llenos o no cumplen con los requisitos-->
-                  <b-button type="submit" :disabled="acceptableSub">Iniciar sesión</b-button>
+                  <b-button type="submit" >Iniciar sesión</b-button>
                 </b-form-group>
                 <b-form-group>
-                  <b-button href="/Login">Registrarse</b-button>
+                  <b-button href="/SignUp">Registrarse</b-button>
                 </b-form-group>
+
+
               </b-form>
+              <b-form-group>
+                <button @click="handleClickGetAuth" :disabled="!isInit">get auth code</button>
+                <button @click="handleClickSignIn" v-if="!isSignIn" :disabled="!isInit">signIn</button>
+                <button @click="handleClickSignOut" v-if="isSignIn" :disabled="!isInit">signOout</button>
+              </b-form-group>
             </b-card-text>
           </b-card>
         </b-col>
@@ -41,6 +54,9 @@
 
 <script>
 
+import User from "@/models/User";
+
+
 export default {
   name: "Login",
   components: {
@@ -48,22 +64,106 @@ export default {
   },
   data(){
     return{
-      Correo: '',
-      Password:''
+      password:'',
+      username:'',
+      user: new User('',''),
+      loading: false,
+      message: '',
+      isInit: false,
+      isSignIn: false
     }
   },
-  methods:{
-    // eslint-disable-next-line no-unused-vars
-    onSubmit(event){
-      alert(this.Correo + ' '+this.Password )
+
+  methods: {
+    onSubmit() {
+      this.user.username=this.username
+      this.user.password=this.password
+      alert('bbbbbbbbbbbbbb')
+      this.loading = true;
+      this.$validator.validateAll().then(() => {
+        // if (!isValid) {
+        //   alert('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        //   this.loading = false;
+        //   return;
+        //
+        // }
+
+        if (this.user.username && this.user.password) {
+          this.$store.dispatch('auth/login', this.user).then(
+              () => {
+                alert('praise the sun')
+                this.$router.push('/List');
+              },
+              error => {
+                this.loading = false;
+                this.message =
+                    (error.response && error.response.data) ||
+                    error.message ||
+                    error.toString();
+              }
+          );
+        }
+      });
+    },
+    handleClickGetAuth(){
+      this.$gAuth.getAuthCode()
+          .then(authCode => {
+            // On success
+            alert(authCode)
+            // return this.$http.post('http://your-backend-server.com/auth/google', { code: authCode, redirect_uri: 'postmessage' })
+          })
+          .then(() => {
+            // And then
+          })
+          .catch(() => {
+            // On fail do something
+          })
+    },
+    handleClickSignIn(){
+      this.$gAuth.signIn()
+          .then(() => {
+            // On success do something, refer to https://developers.google.com/api-client-library/javascript/reference/referencedocs#googleusergetid
+            // console.log('user', GoogleUser)
+            this.isSignIn = this.$gAuth.isAuthorized
+          })
+          .catch(()  => {
+            // On fail do something
+          })
+    },
+    handleClickSignOut(){
+      this.$gAuth.signOut()
+          .then(() => {
+            // On success do something
+            this.isSignIn = this.$gAuth.isAuthorized
+          })
+          .catch(()  => {
+            // On fail do something
+          })
     }
   },
+  mounted(){
+    let that = this
+    let checkGauthLoad = setInterval(function(){
+      that.isInit = that.$gAuth.isInit
+      that.isSignIn = that.$gAuth.isAuthorized
+      if(that.isInit) clearInterval(checkGauthLoad)
+    }, 1000);
+  },
+
   computed:{
-    acceptableSub(){
+    /*acceptableSub(){
       return this.Correo.length > 0 && this.Password.length > 0 ? false:true
+    },*/
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
     }
+  },
+    created() {
+    if (this.loggedIn) {
+      this.$router.push('/profile');
+    }
+  },
   }
-}
 </script>
 
 <style scoped>
